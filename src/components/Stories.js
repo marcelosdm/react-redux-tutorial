f25f612f519a4d6f6b1c7c88c4142ab637b97f6d
Marcelo Melo <marcelosdm@gmail.com>
2019-10-25 11:05:42 -0300

    Add styles

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -3,11 +3,44 @@ import "./Stories.css";
 
 import Story from "./Story";
 
+const COLUMNS = {
+  title: {
+    label: "Title",
+    width: "40%"
+  },
+  author: {
+    label: "Author",
+    width: "30%"
+  },
+  comments: {
+    label: "Comments",
+    width: "10%"
+  },
+  points: {
+    label: "Points",
+    width: "10%"
+  },
+  archive: {
+    width: "10%"
+  }
+};
+
 const Stories = ({ stories }) => (
   <div className="stories">
+    <StoriesHeader columns={COLUMNS} />
     {(stories || []).map(story => (
-      <Story key={story.objectID} story={story} />
+      <Story key={story.objectID} story={story} columns={COLUMNS} />
     ))}
+  </div>
+);
+
+const StoriesHeader = ({ columns }) => (
+  <div className="stories-header">
+    {Object.keys(COLUMNS).map(key => {
+      <span key={key} style={{ width: COLUMNS[key].width }}>
+        {COLUMNS[key].label}
+      </span>;
+    })}
   </div>
 );
 
