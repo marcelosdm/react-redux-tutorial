c3ed4d70c8d7b98a366a42ca097062b979f2b7ec
Marcelo Melo <marcelosdm@gmail.com>
2019-10-25 10:41:02 -0300

    Add Stories with local properties

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import App from "./components/App";
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+const stories = [
+  {
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: "Redux",
+    url: "https://github.com/reactjs/redux",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App stories={stories} />, rootElement);
