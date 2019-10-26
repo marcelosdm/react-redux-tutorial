5dedca5c69fb0b6526b6a6639eb3145a695ad45d
Marcelo Melo <marcelosdm@gmail.com>
2019-10-26 18:53:26 -0300

    Add story reducer

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import store from "./store";
 import "./styles.css";
 
-const stories = [
-  {
-    title: "React",
-    url: "https://facebook.github.io/react/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "https://github.com/reactjs/redux",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
-
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App stories={stories} onArchive={() => {}} />, rootElement);
+ReactDOM.render(
+  <App stories={store.getState()} onArchive={() => {}} />,
+  rootElement
+);
